17682bf5c827d848cdb7d626d90439b3021929be
Sjouke <38879865+Sjouks@users.noreply.github.com>
2024-01-16 13:48:39 +0100

    misc: change to super important sum

---
+++ code/super_important_summation.m
@@ -0,0 +1,8 @@
+function sum = super_important_summation(a, b)
+%SUPER_IMPORTANT_SUMMATION returns the sum of two input variables. 
+
+% Copyright 2022 The MathWorks, Inc.
+
+sum = a + b;
+
+end
--- tests/TestExamples.m
+++ tests/TestExamples.m
@@ -19,68 +19,24 @@ classdef TestExamples < matlab.unittest.TestCase
 
     methods (Test)
 
-        function testNonLeapYear(testCase)
-            % Create non-leap year date of March 1st, 2021
-            dateStr = "03/01/2021";
+        function testSuperImportantSummation(testCase)
+            % Define two example inputs that will be summed and verify if
+            % sum is equal to the expected sum.
 
-            % Calculate expected result
-            dt = datetime(dateStr,"Format","MM/dd/uuuu");
-            doyExpected = day(dt,"dayofyear");
+            % Input
+            a = 3;
+            b = 4;
 
-            % Get actual result
-            doyActual = dayofyear(dateStr);
+            % What we expect the sum to be.
+            expected_sum = 7;
 
-            % Verify that the two are equal
-            testCase.verifyEqual(doyActual,doyExpected)
+            % Calculate super important sum
+            sum = super_important_summation(a,b);
+
+            % Verify that the sum is equal to the expected sum
+            testCase.verifyEqual(sum, expected_sum)
         end
 
-        function testLeapYear(testCase)
-            % Create leap year date of March 1st, 2020
-            dateStr = "03/01/2020";
-
-            % Calculate expected result
-            dt = datetime(dateStr,"Format","MM/dd/uuuu");
-            doyExpected = day(dt,"dayofyear");
-
-            % Get actual result
-            doyActual = dayofyear(dateStr);
-
-            % Verify that the two are equal
-            testCase.verifyEqual(doyActual,doyExpected)
-        end
-
-        function testInvalidDateFormat(testCase)
-            % Create invalid date of April 1st, 2021
-            dateStr = "04-01-2021";
-
-            % Verify that our function throws an error
-            testCase.verifyError(@() dayofyear(dateStr),"dayofyear:InvalidDateFormat");
-        end
-
-        function testCorrectDateFormatButInvalidDate(testCase)
-            % Create invalid date of February 30th, 2021
-            dateStr = "02/30/2021";
-
-            % Verify that our function throws an error
-            testCase.verifyError(@() dayofyear(dateStr),"MATLAB:datetime:ParseErr");
-        end
-
-%         function testAlternateDateFormat(testCase)
-%             % Create date of April 1st, 2021 in alternate date format
-%             dateStr = "01/04/2021";
-%             dateFormat = "dd/mm/yyyy";
-% 
-%             % Calculate expected result
-%             dt = datetime(dateStr,"Format","dd/MM/uuuu");
-%             doyExpected = day(dt,"dayofyear");
-% 
-%             % Get actual result
-%             doyActual = dayofyear(dateStr,dateFormat);
-% 
-%             % Verify that the two are equal
-%             testCase.verifyEqual(doyActual,doyExpected)
-%         end
-        
     end
 
 end
