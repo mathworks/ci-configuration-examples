faafe7f23a389da54fb5aa97f36417551a8f0873
Sjouke <38879865+Sjouks@users.noreply.github.com>
2024-01-16 14:27:36 +0100

    Revert "FIXED"
    This reverts commit eeeb0322c50b117d578d9df8ebc4448b50f5899d.

--- code/super_important_summation.m
+++ code/super_important_summation.m
@@ -1,6 +1,8 @@
 function sum = super_important_summation(a, b)
 %SUPER_IMPORTANT_SUMMATION returns the sum of two input variables. 
 
-sum = a + b + 1;
+% Copyright 2022 The MathWorks, Inc.
+
+sum = a + b;
 
 end
