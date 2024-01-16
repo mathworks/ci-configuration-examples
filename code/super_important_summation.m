eeeb0322c50b117d578d9df8ebc4448b50f5899d
Sjouke <38879865+Sjouks@users.noreply.github.com>
2024-01-16 13:52:03 +0100

    FIXED

--- code/super_important_summation.m
+++ code/super_important_summation.m
@@ -1,8 +1,6 @@
 function sum = super_important_summation(a, b)
 %SUPER_IMPORTANT_SUMMATION returns the sum of two input variables. 
 
-% Copyright 2022 The MathWorks, Inc.
-
-sum = a + b;
+sum = a + b + 1;
 
 end
