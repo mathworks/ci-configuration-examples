42af61f042b3cced3dd8d8798e55c0f2afe33348
Yiting Wang <yitingw@mathworks.com>
2024-02-06 17:15:58 -0500

    update buildfile

--- buildfile.m
+++ buildfile.m
@@ -1,24 +1,26 @@
 function plan = buildfile
+
+import matlab.buildtool.tasks.TestTask
+import matlab.buildtool.tasks.CleanTask
+
+% Create a plan from task functions
 plan = buildplan(localfunctions);
+
+% Add a task to run tests
+plan("test") = TestTask(SourceFiles="code", ...
+    TestResults="test-results/results.html", ...
+    CodeCoverageResults="code-coverage/results.xml");
+
+% Set default task
+plan.DefaultTasks = "test";
+
+plan("clean") = CleanTask;
+plan("test").Dependencies = "setUp";
 
 end
 
-function testTask(context)
-% Run all tests
-testFolder = fullfile(context.Plan.RootFolder, "tests");
+function setUpTask(context)
 srcFolder = fullfile(context.Plan.RootFolder, "code");
-
-% Add code folder to MATLAB search path
-origPath = addpath(srcFolder);
-disp(testFolder);
-
-% Run tests
-results = runtests(testFolder, ...
-    IncludeSubfolders = true, ...
-    OutputDetail = "terse");
-results.assertSuccess;
-
-% Reset the original path
-path(origPath);
+addpath(srcFolder);
 end
 
