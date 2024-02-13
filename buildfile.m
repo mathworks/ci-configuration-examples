25a7d6b35ff67945e1704baba1b39a1b5eb73c86
Yiting Wang <yitingw@mathworks.com>
2024-02-13 15:32:16 -0500

    address feedback

--- buildfile.m
+++ buildfile.m
@@ -1,30 +1,13 @@
 function plan = buildfile
 
-import matlab.buildtool.tasks.TestTask
-import matlab.buildtool.tasks.CleanTask
-
-% Create a plan from task functions
-plan = buildplan(localfunctions);
-
-% Add a task to run tests
-plan("test") = TestTask(SourceFiles="code", ...
-    TestResults="test-results/results.html", ...
-    CodeCoverageResults="code-coverage/results.xml");
-
-% Set default task
-plan.DefaultTasks = "test";
-
-% Make the "test" task dependent on the "setUp" task
-plan("clean") = CleanTask;
-plan("test").Dependencies = "setUp";
-
-end
-
-function setUpTask(context)
-
 % Add source code folder to path
-srcFolder = fullfile(context.Plan.RootFolder, "code");
-addpath(srcFolder);
+addpath("code");
+% Create a plan
+plan = buildplan();
+% Add a task to run tests
+plan("test") = matlab.buildtool.tasks.TestTask("tests");
+% Make the "test" task the default task in the plan
+plan.DefaultTasks = "test";
 
 end
 
