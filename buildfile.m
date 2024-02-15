4e642ff25e9aa2a2a0ffe0f4880f479c312dd8c3
Yiting Wang <yitingw@mathworks.com>
2024-02-15 14:47:33 -0500

    address feedback 2

--- buildfile.m
+++ buildfile.m
@@ -1,11 +1,14 @@
 function plan = buildfile
 
-% Add source code folder to path
+% Add the source folder to the path
 addpath("code");
+
 % Create a plan
-plan = buildplan();
+plan = buildplan(localfunctions);
+
 % Add a task to run tests
 plan("test") = matlab.buildtool.tasks.TestTask("tests");
+
 % Make the "test" task the default task in the plan
 plan.DefaultTasks = "test";
 
