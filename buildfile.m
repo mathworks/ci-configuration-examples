05d01ddbedd00d3985875ba5ddecdc1bc1bdcc16
mw-jsgeorge <73989764+mw-jsgeorge@users.noreply.github.com>
2024-02-29 11:30:00 +0530

    Reverting unintended change to buildfile.m

--- buildfile.m
+++ buildfile.m
@@ -9,10 +9,8 @@ plan = buildplan(localfunctions);
 % Add a task to run tests
 plan("test") = matlab.buildtool.tasks.TestTask("tests");
 
-plan("check") = CodeIssuesTask;
-
 % Make the "test" task the default task in the plan
-plan.DefaultTasks = "check test";
+plan.DefaultTasks = "test";
 
 end
 
